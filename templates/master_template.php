b72b089dc2e3620520dcfa91a6620a3f70929832
Henno Taht <henno.taht@khk.ee>
2014-10-20 13:59:04 +0300

    Add front page

--- templates/master_template.php
+++ templates/master_template.php
@@ -1,106 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<base href="<?=BASE_URL?>">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="shortcut icon" href="assets/ico/favicon.png">
+    <base href="<?= BASE_URL ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+    <script src="assets/components/jquery/1.10.2/jquery-1.10.2.min.js"></script>
+    <script src="assets/components/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 
-	<title><?=PROJECT_NAME?></title>
+    <title><?= PROJECT_NAME ?></title>
 
-	<!-- Bootstrap core CSS -->
-	<link href="assets/components/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/components/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- Custom styles for this template -->
-	<style>
-		body {
-			min-height: 2000px !important;
-			padding-top: 70px;
-		}
-	</style>
+    <!-- Custom styles for this template -->
+    <style>
+        body {
+            padding-top: 70px;
+        }
+    </style>
 
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 
 
 </head>
 
 <body>
-<div class="container">
-    <div class="row">
+<div>
+    <div class="container" style="max-width:1280px">
 
-    </div>
-</div>
-<!-- Fixed navbar -->
-<div class="navbar navbar-default navbar-fixed-top">
-	<div class="col-md-6 col-md-offset-4">
-        <div class="row">
-		<div class="navbar-header">
-
-			<a class="navbar-brand" href="#"><?=PROJECT_NAME?></a>
-		</div>
-		<div class="navbar-collapse collapse">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Avaleht</a></li>
-				<li><a href="#services">Teenused</a></li>
-				<li><a href="#portfolio">Tehtud tööd</a></li>
-				<li><a href="#contact">Kontakt</a></li>
-
-			</ul>
-
-		</div><!--/.nav-collapse -->
-        </div>
-	</div>
-</div>
-
-<div class="container">
-
-	<!-- Main component for a primary marketing message or call to action -->
-	<? if( !file_exists("views/$controller/{$controller}_$action.php")) error_out('The view <i>views/'. $controller . '/' .  $controller . '_' . $action . '.php</i> does not exist. Create that file.');?>
-    <?  @require "views/$controller/{$controller}_$action.php"; ?>
-    <footer>
-        <div class="row">
-          <div class="col-md-4">
-            <ul>
-                <li><b>Atomic AD OÜ</b></li>
-                <li>Atomic AD OÜ</li>
-                <li>Atomic AD OÜ</li>
-                <li>Atomic AD OÜ</li>
-            </ul>
-          </div>
-        </div>
 
         <div class="row">
-            <div class="col-md-4">
-            <ul>
-                <li class="active"><a href="#">Avaleht</a></li>
-                <li><a href="#services">Teenused</a></li>
-                <li><a href="#portfolio">Tehtud tööd</a></li>
-                <li><a href="#contact">Kontakt</a></li>
+            <div class="col-md-1">
 
-            </ul>
-        </div>
             </div>
+            <div class="col-md-10">
+                <div class="row">
+                    <nav class="navbar navbar-default" role="navigation">
+                        <div class="container-fluid">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                        data-target="#bs-example-navbar-collapse-1">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="#">Atomic AD OÜ</a>
+                            </div>
 
-        <div class="row">
-            <div class="col-md-4">
-            <p>copyright, dont steal, i will find you!</p>
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="#">Avaleht</a></li>
+                                    <li><a href="#services">Teenused</a></li>
+                                    <li><a href="#portfolio">Tehtud tööd</a></li>
+                                    <li><a href="#contact">Kontakt</a></li>
+                                </ul>
+                            </div>
+                            <!-- /.navbar-collapse -->
+                        </div>
+                        <!-- /.container-fluid -->
+                    </nav>
+                </div>
+                <div class="row">
+                    <!-- Main component for a primary marketing message or call to action -->
+                    <? if (!file_exists("views/$controller/{$controller}_$action.php")) error_out('The view <i>views/' . $controller . '/' . $controller . '_' . $action . '.php</i> does not exist. Create that file.'); ?>
+                    <? @require "views/$controller/{$controller}_$action.php"; ?>
+                </div>
+                <div class="row">
+                    <footer>
+                            <div class="col-md-4">
+                                <h1>Atomic AD</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolor doloremque est laborum nam quo reiciendis rem, sint veniam voluptates!</p>
+                            </div>
+                            <div class="col-md-4">
+                                <ul class="nav navbar-nav">
+                                    <span class="active"><a href="#">Avaleht</a></span>
+                                    <span><a href="#services">Teenused</a></span>
+                                    <span><a href="#portfolio">Tehtud tööd</a></span>
+                                    <span><a href="#contact">Kontakt</a></span>
+                                </ul>
+                            </div>
+                            <div class="col-md-4">
+                                <p class="help-block">Copyright blabla</p>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+            <div class="col-md-1">
+
+            </div>
         </div>
-        </div>
-    </footer>
-
-</div> <!-- /container -->
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="assets/components/jquery/1.10.2/jquery-1.10.2.min.js"></script>
-<script src="assets/components/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    </div>
+    <!-- /container -->
+</div>
 </body>
 </html>
--- views/home/home_index.php
+++ views/home/home_index.php
@@ -1,7 +1,73 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: henno.taht
- * Date: 20.10.2014
- * Time: 9:34
- */ 
+<div class="row">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="http://placehold.it/1200x315" alt="...">
+                <div class="carousel-caption">
+                    <h3>Caption Text</h3>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://placehold.it/1200x315" alt="...">
+                <div class="carousel-caption">
+                    <h3>Caption Text</h3>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://placehold.it/1200x315" alt="...">
+                <div class="carousel-caption">
+                    <h3>Caption Text</h3>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+    </div> <!-- Carousel -->
+</div>
+<div class="row">
+    <div class="col-md-4">
+    <h1>Atomic AD</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolor doloremque est laborum nam quo reiciendis rem, sint veniam voluptates!</p>
+    </div>
+    <div class="col-md-4">
+        <h1>Teenused</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolor doloremque est laborum nam quo reiciendis rem, sint veniam voluptates!</p>
+
+    </div>
+    <div class="col-md-4">
+        <h1>Kontakt</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolor doloremque est laborum nam quo reiciendis rem, sint veniam voluptates!</p>
+
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <h1>Tehtud tööd</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolor doloremque est laborum nam quo reiciendis rem, sint veniam voluptates!</p>
+    </div>
+    <div class="col-md-8">
+        <img src="http://placehold.it/150x150" alt="img"/>
+        <img src="http://placehold.it/150x150" alt="img"/>
+        <img src="http://placehold.it/150x150" alt="img"/>
+        <img src="http://placehold.it/150x150" alt="img"/>
+    </div>
+</div>
+<script>
+    $('.carousel').carousel({
+        interval: 2000
+    })
+</script>
