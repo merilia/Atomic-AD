5f701fbfe0a31e9688dabc4acd3c4315f0340e61
Merili <meriliasu@gmail.com>
2014-11-05 15:04:29 +0200

    Add portfolio user management

--- views/portfolio/portfolio_index.php
+++ views/portfolio/portfolio_index.php
@@ -1,27 +1,59 @@
 <style>
-    .modal-dialog {width:600px;}
-    .thumbnail {margin-bottom:6px;}
+    .modal-dialog {
+        width: 600px;
+    }
+
+    .thumbnail {
+        margin-bottom: 6px;
+    }
 </style>
 
 <div class="container" style="max-width:1000px">
     <div class="row">
         <h1>Tehtud tööd</h1>
+
         <h2>Klõpsa pildile, et näha seda suuremalt!</h2>
+
         <div class="row">
             <div class="col-md-12" style="margin:0 auto">
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 1"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 2"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 3"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 4"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 5"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 6"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 8"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 9"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 10"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 11"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 12"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-            <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 13"><img src="//placehold.it/700x400" class="thumbnail img-responsive"></a></div>
-        </div>
+
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 1"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 2"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 3"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 4"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 5"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 6"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 8"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 9"><img src="//placehold.it/700x400"
+                                                                                      class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 10"><img src="//placehold.it/700x400"
+                                                                                       class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 11"><img src="//placehold.it/700x400"
+                                                                                       class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 12"><img src="//placehold.it/700x400"
+                                                                                       class="thumbnail img-responsive"></a>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-6"><a href="#" title="Image 13"><img src="//placehold.it/700x400"
+                                                                                       class="thumbnail img-responsive"></a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -42,38 +74,37 @@
     </div>
 </div>
 <script>
-    $('.thumbnail').click(function(){
+    $('.thumbnail').click(function () {
         $('.modal-body').empty();
         var title = $(this).parent('a').attr("title");
         $('.modal-title').html(title);
         $($(this).parents('div').html()).appendTo('.modal-body');
-        $('#myModal').modal({show:true});
+        $('#myModal').modal({show: true});
         return false;
     });
 </script>
-
-<div class="col-md-6">
-    <span class="lead">Laadi üles:</span>
-    <div class="pull-right">
-        <div class="btn-group btn-group-lg">
-            <button type="button" class="btn btn-default" id="btn-photo-upload">Lisa fail...</button>
+<? if($auth->logged_in): ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="pull-right">
+            <div class="btn-group btn-group-lg">
+                <span class="lead" style="float: left; margin-right:20px; margin-top:10px">Laadi üles:</span>
+                <button type="button" class="btn btn-default" id="btn-photo-upload">Lisa fail...</button>
+            </div>
         </div>
-    </div>
-    <form id="uploadForm" method="post" enctype="multipart/form-data" style=" display: none">
-        <input type="file" name="photo" id="text-photo-upload" class="file-upload"/>
-    </form>
-    <pre>
-    <?php var_dump($_FILES) ?>
-    <?php var_dump(get_defined_vars()) ?>
-    </pre>
-    <script>
-        $('#btn-photo-upload').click(function (event) {
-            $('#text-photo-upload').click();
-        });
-        //capture selected filename
-        $('#text-photo-upload').change(function (click) {
+        <form id="uploadForm" method="post" enctype="multipart/form-data" style=" display: none">
+            <input type="file" name="photo" id="text-photo-upload" class="file-upload"/>
+        </form>
+        <script>
+            $('#btn-photo-upload').click(function (event) {
+                $('#text-photo-upload').click();
+            });
+            //capture selected filename
+            $('#text-photo-upload').change(function (click) {
 //  $('#file-name').val(this.value);
-            $('form#uploadForm').submit();
-        });
-    </script>
+                $('form#uploadForm').submit();
+            });
+        </script>
+    </div>
 </div>
+<? endif; ?>
