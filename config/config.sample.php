70275ae3de570bc3a3121d23473d15dcfb6b6165
Merili <meriliasu@gmail.com>
2014-11-05 15:32:10 +0200

    Add PHPMailer

--- config/config.sample.php
+++ config/config.sample.php
@@ -4,3 +4,12 @@ define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
 define('DATABASE_DATABASE', PROJECT_NAME);
+
+define('EMAIL_HOSTNAME', 'smtp.gmail.com');
+define('EMAIL_USERNAME', '');
+define('EMAIL_PASSWORD', '');
+define('EMAIL_SSL', true);
+define('EMAIL_PORT', 465);
+define('EMAIL_AUTH', true);
+define('EMAIL_EMAIL', '');
+define('EMAIL_NAME', '');
