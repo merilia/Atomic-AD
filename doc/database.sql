467e22329b10e444fa22eb72a3eba5c5b9ffe1ea
Merili <meriliasu@gmail.com>
2014-11-17 14:04:23 +0200

    Database dump

--- doc/database.sql
+++ doc/database.sql
@@ -1,19 +1,44 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Loomise aeg: Sept 29, 2014 kell 03:59 PM
--- Serveri versioon: 5.5.34
--- PHP versioon: 5.5.10
+-- Host: 127.0.0.1
+-- Loomise aeg: Nov 17, 2014 kell 01:03 PM
+-- Serveri versioon: 5.5.39
+-- PHP versioon: 5.4.31
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Andmebaas: `halo`
+-- Andmebaas: `atomic-ad`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+`project_id` int(10) unsigned NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `project_file` varchar(255) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=7 ;
+
+--
+-- Andmete tõmmistamine tabelile `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_name`, `project_file`) VALUES
+(1, 'afadg', 'insta.jpg'),
+(2, 'asfafs', 'insta.jpg'),
+(3, '', 'insta.jpg'),
+(4, 'sdad', 'insta.jpg'),
+(5, 'mypictr_last.fm.jpg', 'mypictr_last.fm.jpg'),
+(6, 'asdasd', 'alicerules instagram.PNG');
 
 -- --------------------------------------------------------
 
@@ -22,16 +47,14 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user` (
+`user_id` int(10) unsigned NOT NULL,
   `username` varchar(25) NOT NULL,
   `is_admin` tinyint(4) NOT NULL DEFAULT '0',
   `password` varchar(255) NOT NULL,
   `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL,
-  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `UNIQUE` (`username`)
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
@@ -40,4 +63,34 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `username`, `is_admin`, `password`, `active`, `email`, `deleted`) VALUES
 (1, 'demo', 0, 'demo', 1, '', 0);
-SET FOREIGN_KEY_CHECKS=1;
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `project`
+--
+ALTER TABLE `project`
+ ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indeksid tabelile `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `UNIQUE` (`username`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `project`
+--
+ALTER TABLE `project`
+MODIFY `project_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT tabelile `user`
+--
+ALTER TABLE `user`
+MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;SET FOREIGN_KEY_CHECKS=1;
